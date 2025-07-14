4a9928876e2a2413a680541af5e17564535da827
Gaurav Shonkey <gaurav.shonkey@gmail.com>
2025-07-14 16:43:21 +0530

    backend added

--- rgmain.tf
+++ rgmain.tf
@@ -3,6 +3,14 @@ provider "azurerm" {
     features {}
     subscription_id = "6f5b9c3c-7e8b-46f5-a7be-c171cf606a91"
 }
+terraform {
+  backend "azurerm" {
+    container_name         = "test"
+    key                   = "classic.tfstate"
+    resource_group_name   = "rgstate"
+    storage_account_name  = "storagestate2"
+  }
+}
 
 resource "azurerm_resource_group" "rg1" {
     name = "rg1_westus"
